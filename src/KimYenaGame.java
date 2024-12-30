00873bb8aacc2724bb673c8c69d52016aa8329e1
YENA <130546842+Yenakim242@users.noreply.github.com>
2024-12-30 11:34:45 +0100

    KimYenaGame.java updated

--- src/KimYenaGame.java
+++ src/KimYenaGame.java
@@ -14,4 +14,61 @@ public class KimYenaGame {
     public List<String> loadMoviesFromFile(String filePath) throws IOException {
         return Files.readAllLines(Paths.get(filePath));
     }
+
+    // Método para seleccionar una película aleatoria
+    public void selectRandomMovie(List<String> movies) {
+        Random random = new Random();
+        movieTitle = movies.get(random.nextInt(movies.size())).toLowerCase();
+    }
+
+    // Método para mostrar el título oculto
+    public String displayMaskedTitle() {
+        StringBuilder maskedTitle = new StringBuilder();
+        for (char c : movieTitle.toCharArray()) {
+            if (guessedLetters.contains(c) || !Character.isLetter(c)) {
+                maskedTitle.append(c);
+            } else {
+                maskedTitle.append('_');
+            }
+        }
+        return maskedTitle.toString();
+    }
+
+    // Método para advinar una letra
+    public boolean guessLetter(char letter) {
+        if (movieTitle.indexOf(letter) >= 0) {
+            guessedLetters.add(letter);
+            points += 10;
+            return true;
+        } else {
+            wrongLetters.add(letter);
+            remainingAttempts--;
+            points -= 5;
+            return false;
+        }
+    }
+
+    // Método para adivinar el título completo
+    public boolean guessMovie(String guess) {
+        if (movieTitle.equalsIgnoreCase(guess)) {
+            points += 50;
+            return true;
+        } else {
+            remainingAttempts--;
+            return false;
+        }
+    }
+
+    // Método auxiliares
+    public boolean isGameOver() {
+        return remainingAttempts <= 0 || movieTitle.equals(displayMaskedTitle());
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
 }
