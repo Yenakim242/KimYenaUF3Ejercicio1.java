0d82f4dd825e70420a8074afcb57183c9ce69427
YENA <130546842+Yenakim242@users.noreply.github.com>
2024-12-30 01:25:26 +0100

    La clase para gestionar el juego

--- src/KimYenaGame.java
+++ src/KimYenaGame.java
@@ -1,2 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 public class KimYenaGame {
+    private String movieTitle;
+    private Set<Character> guessedLetters = new HashSet<>();
+    private Set<Character> wrongLetters = new HashSet<>();
+    private int remainingAttempts = 5;
+    private int points = 0;
+
+    // Método para cargar peliculas desde un archivo
+    public List<String> loadMoviesFromFile(String filePath) throws IOException {
+        return Files.readAllLines(Paths.get(filePath));
+    }
 }
